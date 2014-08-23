4ceefef63e8705db26306f9992470f466d773d25
ramahadevan <ramahadevan@ebay.com>
2014-08-23 14:50:14 -0700

    Initial Commit

---
+++ tips/tips/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  tips
+//
+//  Created by Mahadevan, Ramesh on 8/23/14.
+//  Copyright (c) 2014 GoliaMania. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    @IBOutlet var billAmount: UITextField!
+    @IBOutlet var selectedTip: UILabel!
+    @IBOutlet var totalAmount: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
