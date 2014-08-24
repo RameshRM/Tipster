e6ee89cc114838c79856a45eef1d197dc931bc47
ramahadevan <ramahadevan@ebay.com>
2014-08-23 19:59:56 -0700

    added full app

--- tips/tips/ViewController.swift
+++ tips/tips/ViewController.swift
@@ -9,19 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    
     @IBOutlet var billAmount: UITextField!
     @IBOutlet var selectedTip: UILabel!
     @IBOutlet var totalAmount: UILabel!
     
+    @IBOutlet var tipPercentagesList: UISegmentedControl!
+    @IBOutlet var tipAmounts: UISegmentedControl!
+    private var _tipPercentages = [18,20,22];
     override func viewDidLoad() {
+        bindValues(0.0, total: 0.0);
         super.viewDidLoad()
     }
-
+    
+    @IBAction func onTipChanged(sender: AnyObject){
+        var thisBillAmountString = NSString(string: billAmount.text);
+        var selectedTipValue = Double(self._tipPercentages[tipPercentagesList.selectedSegmentIndex])/100;
+        var thisBillAmount = thisBillAmountString.doubleValue;
+        var tipValue = (thisBillAmount) * selectedTipValue;
+        bindValues(tipValue, total: (thisBillAmount + tipValue));
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
-
+    
+    @IBAction func onTapped(sender: AnyObject) {
+        view.endEditing(true);
+    }
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true);
+    }
+    
+    func bindValues(tip: Double, total: Double)->Void{
+        selectedTip.text = String(format:"$%.2f",tip);
+        totalAmount.text = String(format:"$%.2f", total);
+        
+    }
 }
 
