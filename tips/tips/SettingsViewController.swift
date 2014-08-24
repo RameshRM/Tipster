deece27b3d1310cecdd668c53aa684325e20e86b
ramahadevan <ramahadevan@ebay.com>
2014-08-23 21:42:24 -0700

    added settings

---
+++ tips/tips/SettingsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SettingsViewController.swift
+//  tips
+//
+//  Created by Mahadevan, Ramesh on 8/23/14.
+//  Copyright (c) 2014 GoliaMania. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func closeSettings(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+}
